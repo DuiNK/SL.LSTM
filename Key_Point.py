9d7982ede7a9b6cae10014093a11be3dbceb8f1b
Dui.NK <DUY.NK193292@sis.hust.edu.vn>
2023-06-23 23:26:19 +0700

    extract keyFrames

--- Key_Point.py
+++ Key_Point.py
@@ -5,8 +5,7 @@ import mediapipe as mp
 import shutil
 from datetime import timedelta
 
-# i.e if video of duration 30 seconds, saves 10 frame per second = 300 frames saved in total
-SAVING_FRAMES_PER_SECOND = 10
+
 
 mp_holistic = mp.solutions.holistic # Holistic model
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities
@@ -55,39 +54,11 @@ def extract_keypoints(results):
 # Path for exported data, numpy arrays
 DATA_PATH = os.path.join('data_raw/five')
 
-# Actions that we try to detect
-#actions = np.array(['A', 'Ă', 'Â', 'B', 'C', 'D', 'Đ', 'E', 'Ê', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'Ô',
-#                    'Ơ', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ư', 'V', 'W', 'X', 'Y', 'Z', 'dau sac', 'dau huyen', 'dau nga',
-#                    'dau hoi', 'dau nang', 'space', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
-
-#actions = np.aray(['M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-
-#  videos worth of data
-no_sequences = 1
-
-# Videos are going to be 30 frames in length
-sequence_length = 80
-
-# Folder start
-start_folder = 350
-
-#creat folder
-
-# for action in actions:
-#     dirmax = np.max(np.array(os.listdir(os.path.join(DATA_PATH, action))).astype(int))
-#     for sequence in range(1,no_sequences+1):
-#         try:
-#             os.makedirs(os.path.join(DATA_PATH, action, str(dirmax+sequence)))
-#         except:
-#             pass
+#ROOT DATA PATH
 root_folder_path = 'data_raw/five'
 
-# Get a list of all items in the directory
-items = os.listdir(root_folder_path)
 
-# Initialize a counter for the number of directories
-
-
+#count the folder in the dir
 def count_max_folders(root_folder):
     # Get a list of all files and directories in the directory
     files_and_directories = os.listdir(root_folder)
@@ -105,20 +76,16 @@ data_key_path = 'data_keypoint'
 
 
 files = os.listdir(root_folder_path)
-# Set the start and end times (in seconds)
 
-# Create a dictionary to store files by their first 3 characters
-files_by_prefix = {}
+# Get the video dimensions and frame rate
+
+
+
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
 
     for file in files:
-        start_time = 0
-        end_time = 1
-        # Set the frame rate
-        frame_rate = 30
-        # Set the frame index
-        frame_index = 0
 
+        #get prefix
         prefix = file[:3]
         file_path = os.path.join(root_folder_path, file)
 
@@ -130,32 +97,50 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
         keypoint_path = os.path.join(data_key_path, prefix)
         max_dir = count_max_folders(keypoint_path)
-        print(f'max_dỉ {max_dir}')
+        # print(f'max_dỉ {max_dir}')
 
         # Open the video file
         cap = cv2.VideoCapture(file_path)
-        while True:
-            # Set the current time (in seconds)
-            current_time = frame_index / frame_rate
+        # frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        # frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        # frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
+        # Get the total number of frames in the video
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        print(f'total frame of {total_frames}')
 
-            # Check if we've reached the end time
-            if current_time > end_time:
-                break
+        # Calculate the frame interval to use for keyframe extraction
+        interval = float(total_frames / 60)
+
+        # Initialize the keyframe list
+        keyframes = []
+        frame_index = 0
+
+        # Define the codec and output file name
+        # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        # output_filename = file + 'output_video.mp4'
+        # out = cv2.VideoWriter(output_filename, fourcc, frame_rate, (frame_width, frame_height))
+        for i in range(total_frames):
+            # Set the current frame position
+            cap.set(cv2.CAP_PROP_POS_FRAMES, i)
 
             # Read the next frame
             ret, frame = cap.read()
 
+
             # Check if the frame was successfully read
             if not ret:
                 break
+            # If this is a keyframe, add it to the list
+            if i == round(interval * frame_index):
+                # keyframes.append(frame)
+                # cv2.imwrite(file_path + str(i) + '.jpg', frame)
+                # out.write(frame)
 
-            # Check if we've reached the start time
-            if current_time >= start_time:
                 # Make detections
                 image, results = mediapipe_detection(frame, holistic)
                 # NEW Export keypoints
                 keypoints = extract_keypoints(results)
-                print("keypoint", keypoints)
+                # print("keypoint", keypoints)
                 if not os.path.exists(os.path.join(data_key_path, prefix, str(max_dir))):
                     os.makedirs(os.path.join(data_key_path, prefix, str(max_dir)))
                     print(f"Created directory at {os.path.join(data_key_path, prefix, str(max_dir))}")
@@ -164,19 +149,14 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
                 npy_path = os.path.join(data_key_path, prefix, str(max_dir), str(frame_index))
                 np.save(npy_path, keypoints)
+                # Increment the frame index
+                frame_index += 1
 
-            # Increment the frame index
-            frame_index += 1
+
+
 
         # Release the video file
         cap.release()
+        # out.release()
 
-
-# Create a folder for each prefix and move the files into the respective folder
-# for prefix, files in files_by_prefix.items():
-#     folder_path = os.path.join(root_folder_path, prefix)
-#     os.makedirs(folder_path, exist_ok=True)
-#     for file in files:
-#         file_path = os.path.join(root_folder_path, file)
-#         folder_file_path = os.path.join(folder_path, file)
 
